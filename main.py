cc235b442ccdb43aeb299d67bf322b7da5e492dc
Nathan Maldonado <maldonadomunoz@gmail.com>
2023-10-29 18:09:52 -0400

    generalized dm

--- main.py
+++ main.py
@@ -106,90 +106,66 @@ squad_rawdata['cd_white'] = ((
 squad_rawdata['cd'] = (((squad_rawdata['cd_green']) + (squad_rawdata['cd_blue']) + (squad_rawdata['cd_white'])) / 3)
 squad_rawdata.cd = squad_rawdata.cd.round(1)
 
-# Defensive Midfielder Score
+# DM Score
 squad_rawdata['dm_green'] = ((
-    (squad_rawdata['Tck'] * 5) +
-    (squad_rawdata['Agg'] * 5) +
-    (squad_rawdata['Ant'] * 5) +
-    (squad_rawdata['Tea'] * 5) +
     (squad_rawdata['Wor'] * 5) +
-    (squad_rawdata['Sta'] * 5)) / 30
+    (squad_rawdata['Sta'] * 5) +
+    (squad_rawdata['Pac'] * 5) +
+    (squad_rawdata['Pas'] * 5) +
+    (squad_rawdata['Vis'] * 5) +
+    (squad_rawdata['Dec'] * 5) +
+    (squad_rawdata['Cmp'] * 5)) / 35
     )
 
 squad_rawdata['dm_blue'] = ((
-    (squad_rawdata['Mar'] * 2.5) +
-    (squad_rawdata['Bra'] * 2.5) +
+    (squad_rawdata['Ant'] * 2.5) +
+    (squad_rawdata['OtB'] * 2.5) +
+    (squad_rawdata['Fir'] * 2.5) +
+    (squad_rawdata['Tec'] * 2.5) +
     (squad_rawdata['Cnt'] * 2.5) +
-    (squad_rawdata['Pos'] * 2.5) +
-    (squad_rawdata['Agi'] * 2.5) +
-    (squad_rawdata['Pac'] * 2.5) +
-    (squad_rawdata['Str'] * 2.5)) / 17.5
+    (squad_rawdata['Acc'] * 2.5) +
+    (squad_rawdata['Pos'] * 2.5)) / 17.5
     )
 
 squad_rawdata['dm_white'] = ((
-    (squad_rawdata['Cro']) +
-    (squad_rawdata['Dri']) +
-    (squad_rawdata['Fin']) +
-    (squad_rawdata['Fir']) +
-    (squad_rawdata['Hea']) +
-    (squad_rawdata['Lon']) +
-    (squad_rawdata['Pas']) +
-    (squad_rawdata['Tec']) +
-    (squad_rawdata['Cmp']) +
-    (squad_rawdata['Dec']) +
-    (squad_rawdata['Det']) +
-    (squad_rawdata['Ldr']) +
-    (squad_rawdata['OtB']) +
-    (squad_rawdata['Vis']) +
-    (squad_rawdata['Acc']) +
+    (squad_rawdata['Tea']) +
     (squad_rawdata['Bal']) +
-    (squad_rawdata['Jum']) +
-    (squad_rawdata['Fla'])) / 18
+    (squad_rawdata['Str']) +
+    (squad_rawdata['Dri']) +
+    (squad_rawdata['Lon']) +
+    (squad_rawdata['Tck']) +
+    (squad_rawdata['Fla']) +
+    (squad_rawdata['Agi'])) / 8
     )
-
 
 squad_rawdata['dm'] = (((squad_rawdata['dm_green']) + (squad_rawdata['dm_blue']) + (squad_rawdata['dm_white'])) / 3)
 squad_rawdata.dm = squad_rawdata.dm.round(1)
 
 # Winger Score
 squad_rawdata['w_green'] = ((
-    (squad_rawdata['Cro'] * 5) +
-    (squad_rawdata['Dri'] * 5) +
-    (squad_rawdata['Tec'] * 5) +
     (squad_rawdata['Acc'] * 5) +
-    (squad_rawdata['Agi'] * 5)) / 25
+    (squad_rawdata['Pac'] * 5) +
+    (squad_rawdata['Sta'] * 5) +
+    (squad_rawdata['Wor'] * 5) +
+    (squad_rawdata['Cro'] * 5) +
+    (squad_rawdata['Dri'] * 5)) / 30
     )
 
 squad_rawdata['w_blue'] = ((
-    (squad_rawdata['Fir'] * 2.5) +
+    (squad_rawdata['Tec'] * 2.5) +
+    (squad_rawdata['Agi'] * 2.5) +
     (squad_rawdata['OtB'] * 2.5) +
-    (squad_rawdata['Pas'] * 2.5) +
-    (squad_rawdata['Bal'] * 2.5) +
-    (squad_rawdata['Pac'] * 2.5) +
-    (squad_rawdata['Sta'] * 2.5) +
-    (squad_rawdata['Wor'] * 2.5)) / 17.5
+    (squad_rawdata['Pas'] * 2.5)) / 10
     )
 
 squad_rawdata['w_white'] = ((
-    (squad_rawdata['Fin']) +
-    (squad_rawdata['Hea']) +
-    (squad_rawdata['Lon']) +
-    (squad_rawdata['Mar']) +
-    (squad_rawdata['Tck']) +
-    (squad_rawdata['Agg']) +
-    (squad_rawdata['Ant']) +
-    (squad_rawdata['Bra']) +
-    (squad_rawdata['Cmp']) +
-    (squad_rawdata['Cnt']) +
-    (squad_rawdata['Dec']) +
-    (squad_rawdata['Det']) +
-    (squad_rawdata['Fla']) +
-    (squad_rawdata['Ldr']) +
+    (squad_rawdata['Fir']) +
+    (squad_rawdata['Bal']) +
     (squad_rawdata['Pos']) +
-    (squad_rawdata['Tea']) +
     (squad_rawdata['Vis']) +
-    (squad_rawdata['Jum']) +
-    (squad_rawdata['Str'])) / 19
+    (squad_rawdata['Ant']) +
+    (squad_rawdata['Cnt']) +
+    (squad_rawdata['Agg'])) / 7
     )
 
 squad_rawdata['w'] = (((squad_rawdata['w_green']) + (squad_rawdata['w_blue']) + (squad_rawdata['w_white'])) / 3)
@@ -197,43 +173,30 @@ squad_rawdata.w = squad_rawdata.w.round(1)
 
 # Advanced Playmaker Score
 squad_rawdata['ap_green'] = ((
-    (squad_rawdata['Fir'] * 5) +
-    (squad_rawdata['Pas'] * 5) +
-    (squad_rawdata['Tec'] * 5) +
+    (squad_rawdata['Acc'] * 5) +
+    (squad_rawdata['Pac'] * 5) +
+    (squad_rawdata['Sta'] * 5) +
+    (squad_rawdata['Wor'] * 5) +
     (squad_rawdata['Cmp'] * 5) +
-    (squad_rawdata['OtB'] * 5) +
-    (squad_rawdata['Tea'] * 5) +
-    (squad_rawdata['Vis'] * 5) +
-    (squad_rawdata['Dec'] * 5)) / 40
+    (squad_rawdata['Dec'] * 5) +
+    (squad_rawdata['Pas'] * 5) +
+    (squad_rawdata['Ant'] * 5)) / 40
     )
 
 squad_rawdata['ap_blue'] = ((
     (squad_rawdata['Dri'] * 2.5) +
-    (squad_rawdata['Ant'] * 2.5) +
+    (squad_rawdata['Fir'] * 2.5) +
     (squad_rawdata['Fla'] * 2.5) +
-    (squad_rawdata['Acc'] * 2.5) +
-    (squad_rawdata['Agi'] * 2.5)) / 12.5
+    (squad_rawdata['Tec'] * 2.5)) / 10
     )
 
 squad_rawdata['ap_white'] = ((
-    (squad_rawdata['Cro']) +
-    (squad_rawdata['Fin']) +
-    (squad_rawdata['Hea']) +
-    (squad_rawdata['Lon']) +
-    (squad_rawdata['Mar']) +
-    (squad_rawdata['Tck']) +
+    (squad_rawdata['Agi']) +
     (squad_rawdata['Agg']) +
-    (squad_rawdata['Bra']) +
-    (squad_rawdata['Det']) +
-    (squad_rawdata['Cnt']) +
-    (squad_rawdata['Ldr']) +
-    (squad_rawdata['Pos']) +
-    (squad_rawdata['Wor']) +
-    (squad_rawdata['Bal']) +
-    (squad_rawdata['Jum']) +
-    (squad_rawdata['Pac']) +
-    (squad_rawdata['Sta']) +
-    (squad_rawdata['Str'])) / 18
+    (squad_rawdata['Fin']) +
+    (squad_rawdata['Vis']) +
+    (squad_rawdata['Tea']) +
+    (squad_rawdata['OtB'])) / 6
     )
 
 squad_rawdata['ap'] = (((squad_rawdata['ap_green']) + (squad_rawdata['ap_blue']) + (squad_rawdata['ap_white'])) / 3)
@@ -241,43 +204,30 @@ squad_rawdata.ap = squad_rawdata.ap.round(1)
 
 # Pressing Forward on Attack Score
 squad_rawdata['pf_green'] = ((
-    (squad_rawdata['Agg'] * 5) +
-    (squad_rawdata['Ant'] * 5) +
-    (squad_rawdata['Bra'] * 5) +
-    (squad_rawdata['OtB'] * 5) +
-    (squad_rawdata['Tea'] * 5) + 
-    (squad_rawdata['Wor'] * 5) +   
     (squad_rawdata['Acc'] * 5) +
+    (squad_rawdata['Dri'] * 5) +
     (squad_rawdata['Pac'] * 5) +
-    (squad_rawdata['Sta'] * 5)) / 45
+    (squad_rawdata['Cmp'] * 5) +
+    (squad_rawdata['Fin'] * 5) +
+    (squad_rawdata['Wor'] * 5) +   
+    (squad_rawdata['Sta'] * 5)) / 35
     )
 
 squad_rawdata['pf_blue'] = ((
-    (squad_rawdata['Fin'] * 2.5) +
     (squad_rawdata['Fir'] * 2.5) +
-    (squad_rawdata['Cmp'] * 2.5) +
-    (squad_rawdata['Cnt'] * 2.5) +
+    (squad_rawdata['Agg'] * 2.5) +
+    (squad_rawdata['Ant'] * 2.5) +
     (squad_rawdata['Dec'] * 2.5) +
-    (squad_rawdata['Agi'] * 2.5) +
-    (squad_rawdata['Bal'] * 2.5) +
-    (squad_rawdata['Str'] * 2.5)) / 20
+    (squad_rawdata['OtB'] * 2.5) +
+    (squad_rawdata['Bal'] * 2.5)) / 15
     )
 
 squad_rawdata['pf_white'] = ((
-    (squad_rawdata['Cro']) +
-    (squad_rawdata['Dri']) +
-    (squad_rawdata['Hea']) +
-    (squad_rawdata['Lon']) +
-    (squad_rawdata['Mar']) +
-    (squad_rawdata['Pas']) +
-    (squad_rawdata['Tck']) +
-    (squad_rawdata['Tec']) +
-    (squad_rawdata['Det']) +
-    (squad_rawdata['Fla']) +
-    (squad_rawdata['Ldr']) +
-    (squad_rawdata['Pos']) +
-    (squad_rawdata['Vis']) +
-    (squad_rawdata['Jum'])) / 14
+    (squad_rawdata['Bra']) +
+    (squad_rawdata['Cnt']) +
+    (squad_rawdata['Tea']) + 
+    (squad_rawdata['Str']) +
+    (squad_rawdata['Agi'])) / 5
     )
 
 squad_rawdata['pf'] = (((squad_rawdata['pf_green']) + (squad_rawdata['pf_blue']) + (squad_rawdata['pf_white'])) / 3)
